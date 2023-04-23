aab4c8b7ed11f6f4d487780111499d07c8ff301d
BLY117 <brendon.leeying@my.uwi.edu>
2023-04-23 02:49:49 +0000

    modified:   App/main.py 	modified:   App/models/user.py 	modified:   App/static/style.css 	new file:   App/templates/home.html 	deleted:    App/templates/index.html 	modified:   App/templates/layout.html 	new file:   App/templates/login.html 	new file:   App/templates/signup.html 	modified:   App/views/auth.py 	modified:   App/views/index.py 	modified:   App/views/user.py 	new file:   competitions.csv 	modified:   wsgi.py

--- App/views/user.py
+++ App/views/user.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user
+from App.models import Competition, CompetitonUser, User, db
 
 from.index import index_views
 
@@ -12,10 +13,31 @@ from App.controllers import (
 
 user_views = Blueprint('user_views', __name__, template_folder='../templates')
 
+@user_views.route('/', methods=['GET'])
+def get_home_page():
+    competitions = Competition.query.all()
+    return render_template("home.html", competitions=competitions)
+
 @user_views.route('/users', methods=['GET'])
 def get_user_page():
     users = get_all_users()
     return render_template('users.html', users=users)
+
+@user_views.route('/login', methods=['GET'])
+def get_login_page():
+    users = get_all_users()
+    return render_template('login.html', users=users)
+
+@user_views.route("/logout", methods=['GET'])
+def logout_action():
+  #logout_user()
+  flash('Logged Out')
+  return redirect(url_for('user_views.get_login_page'))
+
+@user_views.route('/signup', methods=['GET'])
+def get_signup_page():
+    users = get_all_users()
+    return render_template('signup.html', users=users)
 
 @user_views.route('/api/users', methods=['GET'])
 def get_users_action():
@@ -28,13 +50,17 @@ def create_user_endpoint():
     create_user(data['username'], data['password'])
     return jsonify({'message': f"user {data['username']} created"})
 
-@user_views.route('/api/login', methods=['POST'])
+@user_views.route('/login', methods=['POST'])
 def user_login_api():
-  data = request.json
-  token = authenticate(data['username'], data['password'])
-  if not token:
-    return jsonify(message='bad username or password given'), 401
-  return jsonify(access_token=token)
+    data = request.json
+    user = User.query.filter_by(username=data['username']).first()
+    if user and user.check_password(data['password']):
+        flash('Logged in successfully.')
+        #login_user(user)
+        return redirect(url_for('user_views.get_home_page'))
+    else:
+        return jsonify(message='bad username or password given'), 401
+    return redirect('/login')
 
 @user_views.route('/api/identify', methods=['GET'])
 @jwt_required()
